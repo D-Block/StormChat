c371c5811d222222e7559cbbffc4eeb83f639105
Nick <xdedalusx@gmail.com>
2016-10-20 22:54:25 -0400

    Initial commit

---
+++ wear/src/main/java/tech/horiz0n/stormchat/stormchat/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package tech.horiz0n.stormchat.stormchat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("tech.horiz0n.stormchat.stormchat.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
